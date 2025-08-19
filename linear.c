59bdcba37c1d241f1aec9729b5a404ce850ba615
akshaysai2006 <156563098+akshaysai2006@users.noreply.github.com>
2025-08-19 12:31:41 +0530

    Update linear.c

--- linear.c
+++ linear.c
@@ -13,17 +13,17 @@ int ls(int arr[],int size,int key)
 int main()
 {
 	int n,key,position;
-	printf("Enter the number of elements\t:);
+	printf("Enter the number of elements\t:");
 	scanf("%d",&n);
 	int arr[n];
-	printf("Enter %d elements:",n);
+	printf("Enter %d elements\n:",n);
 	for (int i=0;i<n;i++)
 	{
 		scanf("%d",&arr[i]);
 	}
 	printf("Enter the element to search:");
 	scanf("%d",&key);
-	position=ls(arr,n,key)
+	position=ls(arr,n,key);
 	if (position==-1)
 	{
 	printf("Element %d not found in the array.\n",key);	
